c0b6cee8b7533361a0c4cddd11fff8dfb564edb2
Fer Nanda <fermixmax_452@hotmail.com>
2018-09-13 23:30:14 -0300

    Intento de Algo

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ListaProducto.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ListaProducto.java
@@ -7,9 +7,9 @@ import android.widget.ArrayAdapter;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
 
 public class ListaProducto extends AppCompatActivity {
-    final Categoria[] datos = new Categoria[];
+    final Categoria[] datos = new Categoria[]{};
 
-    ArrayAdapter<Categoria> adaptador = new ArrayAdapter<Categoria>(getApplicationContext(),android.R.layout.simple_list_item_1,datos)
+    ArrayAdapter<Categoria> adaptador = new ArrayAdapter<Categoria>(getApplicationContext(),android.R.layout.simple_list_item_1,datos);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
